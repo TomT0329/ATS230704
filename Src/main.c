7144076985cbf12becf8a42ab59a7cc417bf59eb
Tom Tang <aa159231@gmail.com>
2023-11-24 13:42:40 +0800

    Increase deadtime to 8us

--- Src/main.c
+++ Src/main.c
@@ -425,7 +425,7 @@ static void MX_TIM1_Init(void)
   sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_ENABLE;
   sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_ENABLE;
   sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
-  sBreakDeadTimeConfig.DeadTime = ((DEAD_TIME_COUNTS) / 2);
+  sBreakDeadTimeConfig.DeadTime = ((DEAD_TIME_COUNTS) * 20);
   sBreakDeadTimeConfig.BreakState = TIM_BREAK_ENABLE;
   sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
   sBreakDeadTimeConfig.BreakFilter = 3;
